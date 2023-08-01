ee877b1f074482384e20f5371f3f43bc733ed700
pramila patel <pramilapatel095@gmail.com>
2023-08-01 08:40:58 +0530

    header

---
+++ include/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+       
+        <ul>
+            <img src="images (11).jfif" alt="" >
+            <li> <a href="#"> Home </a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#"> Services</a></li>
+            <li><a href="#"> Content</a></li>
+            <li><a href="#"> Gallary</a></li>
+        </ul>
+    </div>
