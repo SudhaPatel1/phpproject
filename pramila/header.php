7209e93910baa50212e791baef5aa809194ee38f
pramila patel <pramilapatel095@gmail.com>
2023-08-01 09:20:19 +0530

    pramila

---
+++ pramila/header.php
@@ -0,0 +1,11 @@
+
+<body>
+    <div class="header">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Gallery</li>
+            <li>Contect</li>
+        </ul>
+    </div>
+ 
